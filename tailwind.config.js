a426332ea5f7a2674b494f8f90117b6b70bf08c8
Armaan Singh <sarmaan20720@gmail.com>
2023-02-15 18:30:34 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        LightBlue: "#404eed",
+        BlurBlack: "#23272A",
+        OffWhite: "#f1f5f9",
+        DarkBlue: "#5865f2",
+      },
+
+      fontFamily: {
+        ggsansBold: ["ggsansBold"],
+        ggsansMedium: ["ggsansMedium"],
+        ggsansNormal: ["ggsansNormal"],
+        ggsansSemiBold: ["ggsansSemiBold"],
+        GintoNordNormal: ["GintoNordNormal"],
+        GintoNordNormal2: ["GintoNordNormal2"],
+        GintoNordSemiBold: ["GintoNordSemiBold"],
+        GintoNordSemiBold2: ["intoNordSemiBold2"],
+        GintoNordBold: [" GintoNordBold"],
+        GintoNordBold2: ["GintoNordBold2"],
+        GintoItalicNormal: ["GintoItalicNormal"],
+        GintoNormal: ["GintoNormal"],
+        GintoItalicMedium: ["GintoItalicMedium"],
+        GintoMedium: ["GintoMedium"],
+        GintoItalicSemiBold: ["GinntoItalicSemiBold"],
+        GintoSemiBold: ["GintoSemiBold"],
+        GintoBold: ["GintoBold"],
+        GintoItalicBold: ["GintoItalicBold"],
+        GintoExBold: ["GintoExBold"],
+        GintoItalicExBold: ["GintoItalicExBold"],
+        WhitneyThin: ["WhitneyThin"],
+        WhitneyNormal: ["WhitneyNormal"],
+        WhitneyMedium: ["WhitneyMedium"],
+        WhitneySemiBold: ["WhitneySemiBold"],
+        WhitneyBold: ["WhitneyBold"],
+      },
+    },
+  },
+  plugins: [],
+};
